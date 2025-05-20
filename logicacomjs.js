3bd4873c2ae029bd16232d3b9077a878ea331224
nramxs <nayramos642@gmail.com>
2025-05-20 16:37:37 -0300

    Variaveis,comentario,alert e console log

---
+++ logicacomjs.js
@@ -0,0 +1,25 @@
+/*let homens = 4
+let mulheres = 3
+let soma = homens + mulheres
+console.log("A soma de homens e mulheres é "+homens+"+"+mulheres+"="+ soma)
+console.log("-----------------")
+let alunos = 30
+let uniformes = 28
+let sub = alunos - uniformes
+console.log("A subtração entre alunos e uniformes é de "+alunos+"-"+uniformes+"="+sub)
+console.log("-----------")
+let faturas  = 187
+let parcelas = 5
+let multi = faturas*parcelas
+console.log("A multiplicação entre as faturas e as percelas é de "+faturas+"*"+parcelas+"="+multi)
+console.log("------------")
+let notas = 28
+let avaliações = 4
+let div = notas/avaliações
+console.log("A divisão entre as notas do aluno e as avaliações é de "+notas+"/"+avaliações+"="+div)*/
+
+let salario_bruto = 2800
+let meses_trab = 10
+let décimo = (salario_bruto/12)*meses_trab
+let nome_do_usuário = "José"
+console.log(nome_do_usuário+ "seu décimo é:"+ décimo)
