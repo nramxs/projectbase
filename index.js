fa60f5835bfc864b55d4c31b7263fba1585b4bf1
nramxs <nayramos642@gmail.com>
2025-06-12 17:02:56 -0300

    inicializandodom

---
+++ index.js
@@ -0,0 +1,7 @@
+/*let leitura = document.getElementsByTagName("li")[3]
+leitura.style.color = "gray"
+let leitura = document.getElementsByName("h4dadiv")[0]
+leitura.innerText = "Já fui e voltei!!"
+leitura.style.color = "red"
+let leitura = document.getElementById('atalaia')
+leitura.style.background = "gray"*/
